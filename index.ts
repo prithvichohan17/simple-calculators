3254fb24caed1b921ce6de975426f948fe7cf984
prithvichohan <162011259+prithvichohan17@users.noreply.github.com>
2024-04-10 20:05:22 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+//
+import inquirer from "inquirer";
+
+const asnwer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division",],
+  },
+]);
+
+  //conditional statement
+   if (asnwer. operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+   }
+   else if(asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+   }
+   else if (asnwer. operator === "Multiplication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber); 
+    
+  }else if(asnwer. operator === "Division"){
+    console.log(asnwer.firstNumber / asnwer.secondNumber);
+  }else {
+    console.log("please select valid operator");
+    
+  }
+  
+
+
+
